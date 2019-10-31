75eb855e79fcf104717276a9ad671765fa5a0f67
Kelvinyu <wukuwnyu@gmail.com>
2019-10-31 23:06:00 +0800

    dev

--- src/Tutorial4/ChatGroupTest.java
+++ src/Tutorial4/ChatGroupTest.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+
 public class ChatGroupTest {
     public static List<User> getFriends(List<ChatGroup> groups, User u) {
         // Implement this static method.
